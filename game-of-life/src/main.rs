66cb2574213728d817426faf569d35eeccdce816
Bryan Benson <bryan.benson@gmail.com>
2023-12-26 14:43:26 -0800

    Add camera and background on startup.

--- game-of-life/src/main.rs
+++ game-of-life/src/main.rs
@@ -2,21 +2,49 @@
 
 use bevy::prelude::*;
 
-// Window starting dimensions
-const WINDOW_START_HEIGHT: f32 = 800.0;
-const WINDOW_START_WIDTH: f32 = 700.0;
+//Default for the tile sizes.
+const TILE_SIZE: u16 = 40;
+
+struct Board {
+    squares_wide: u16,
+    squares_high: u16,
+}
+
 fn main() {
     println!("Bevy app starting!");
+    let cols = 20;
+    let rows = 20;
+    let board = Board {squares_wide: cols, squares_high: rows};
+    let window_width = TILE_SIZE * board.squares_wide;
+    let window_height =  TILE_SIZE * board.squares_high;
     App::new()
         .add_plugins(
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
                     title: "Conway's Game of Life".into(),
-                    resolution: (WINDOW_START_WIDTH, WINDOW_START_HEIGHT).into(),
+                    resolution: (f32::from(window_width), f32::from(window_height)).into(),
                     ..default()
                 }),
                 ..default()
             })
         )
+        .add_systems(Startup, initial_setup)
         .run();
+}
+
+fn initial_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    //Draw the background!
+    commands.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            position_type: PositionType::Absolute,
+            justify_content: JustifyContent::SpaceAround,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        background_color: Color::BLUE.into(),
+        ..default()
+    });
 }
