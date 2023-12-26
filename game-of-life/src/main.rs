be1ac31f8bd14eb4f649ba6f6cd13de50d590b70
Bryan Benson <bryan.benson@gmail.com>
2023-12-26 14:17:26 -0800

    Window title and display functionality.

--- game-of-life/src/main.rs
+++ game-of-life/src/main.rs
@@ -1,3 +1,22 @@
+#![warn(clippy::pedantic)]
+
+use bevy::prelude::*;
+
+// Window starting dimensions
+const WINDOW_START_HEIGHT: f32 = 800.0;
+const WINDOW_START_WIDTH: f32 = 700.0;
 fn main() {
-    println!("Hello, world!");
+    println!("Bevy app starting!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Conway's Game of Life".into(),
+                    resolution: (WINDOW_START_WIDTH, WINDOW_START_HEIGHT).into(),
+                    ..default()
+                }),
+                ..default()
+            })
+        )
+        .run();
 }
