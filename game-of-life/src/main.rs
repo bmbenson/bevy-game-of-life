a6440c3b7f849ef495fcf9cbfc46b376bb219a73
Bryan Benson <bryan.benson@gmail.com>
2023-12-26 15:19:51 -0800

    Checker board view.

--- game-of-life/src/main.rs
+++ game-of-life/src/main.rs
@@ -5,6 +5,7 @@ use bevy::prelude::*;
 //Default for the tile sizes.
 const TILE_SIZE: u16 = 40;
 
+#[derive(Resource)]
 struct Board {
     squares_wide: u16,
     squares_high: u16,
@@ -28,23 +29,51 @@ fn main() {
                 ..default()
             })
         )
+        .insert_resource(board)
         .add_systems(Startup, initial_setup)
         .run();
 }
 
-fn initial_setup(mut commands: Commands) {
+fn initial_setup(mut commands: Commands, board: Res<Board>) {
     commands.spawn(Camera2dBundle::default());
-    //Draw the background!
-    commands.spawn(NodeBundle {
-        style: Style {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            position_type: PositionType::Absolute,
-            justify_content: JustifyContent::SpaceAround,
-            align_items: AlignItems::Center,
+    //Draw the grid layout!
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                // Create a grid layout, at 100% of the parent element
+                // Height and width.
+                display: Display::Grid,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                grid_template_columns: vec![
+                    GridTrack::auto(); usize::from(board.squares_wide)
+                ],
+                grid_template_rows: vec![
+                    GridTrack::auto(); usize::from(board.squares_high)
+                ],
+                ..default()
+            },
+            background_color: BackgroundColor(Color::WHITE),
             ..default()
-        },
-        background_color: Color::BLUE.into(),
-        ..default()
-    });
+        })
+        .with_children(|builder| {
+            //Every other will be black or red!
+            for c in 0..board.squares_wide {
+                for r in 0..board.squares_high {
+                    let color = if (r + c) % 2 == 0 {
+                        Color::RED
+                    } else {
+                        Color::BLACK
+                    };
+                    builder.spawn(NodeBundle {
+                        style: Style {
+                            display: Display::Grid,
+                            ..default()
+                        },
+                        background_color: BackgroundColor(color),
+                        ..default()
+                    });
+                }
+            }
+        });
 }
